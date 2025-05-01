d634cb5c6c36c2363d2bfe83273d3a50b70f27de
Matias Perez <matiperez881@gmail.com>
2025-05-01 11:31:41 -0300

    update

--- index.js
+++ index.js
@@ -1,113 +1,90 @@
 const express = require('express');
-const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
-app.use(express.json());
+app.use(express.json()) //activar json-parser para recibir datos en formato JSON
+app.use(express.static('dist'));
+app.use(cors());
 
-app.use(morgan((tokens, req, res) => {
-  const log = [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens['response-time'](req, res), 'ms',
-    req.method === 'POST' ? `Body: ${JSON.stringify(req.body)}` : ''
-  ].join(' | ');
-  return log;
-}));
-
-let persons = [
-  { 
+let notes = [
+  {
     id: 1,
-    name: "Arto Hellas", 
-    number: "040-123456"
+    content: "HTML is easy",
+    important: true
   },
-  { 
+  {
     id: 2,
-    name: "Ada Lovelace", 
-    number: "39-44-5323523"
+    content: "Browser can execute only JavaScript",
+    important: false
   },
-  { 
+  {
     id: 3,
-    name: "Dan Abramov", 
-    number: "12-43-234345"
-  },
-  { 
-    id: 4,
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122"
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
   }
 ]
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons);
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+app.get('/', (request, response) =>{
+  response.send('<h1>Hallo munde!</h1>')
 });
 
-app.get('/info', (request, response) =>{
-  const entradas =`<p>Phonebook has info for ${persons.length+1} people</p>`
-  const espacio = '<br>';
-  const now = new Date();
-  const formattedDate = `<p>${now.toString()}</p>`
-  const mensajeEntero = entradas+espacio+formattedDate;
-  response.send(mensajeEntero);
-
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id= Number(request.params.id);
-  const person = persons.find(person => person.id === id);
-  if(person){
-    response.json(person);
+app.get('/api/notes/:id', (request, response) =>{
+  const id=Number(request.params.id);
+  const note=notes.find(note => note.id===id);
+  if(note){
+    response.json(note);
   }else{
     response.status(404).end();
   }
 });
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !==person);
+  notes = notes.filter(note => note.id !==id);
 
   response.status(204).end();
 })
 
 const generateId = () => {
-  const randomId = Math.floor(Math.random()*1000)+1;
-  return randomId;
-};
+  const maxId = notes.length >0 ? Math.max(...notes.map(n=>n.id)) : 0;
+  return maxId+1;
+}
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/notes', (request, response) => {
   const body = request.body;
 
-  if(!body.name){
+  if(!body.content){
     return response.status(400).json({
       error: 'content missing'
-    });
-  };
-  if(!body.number){
-    return response.status(400).json({
-      error: 'content missing'
-    });
-  };
-
-  if(persons.some(person => person.name === body.name)){
-    return response.status(400).json({
-        error: 'name must be unique'
-    });
+    })
   }
 
-  const person = {
-    name:body.name,
-    number: body.number,
-    id: generateId()
+
+  //const note = request.body; //sin json-parser la propiedad body no estaria definida. 
+  //el json-parser funciona para que toem los datos JSON de una solicitud, los transforme en un objeto javascript y luego los adjunte a la propiedad body del objeto request antes de llamar al controlador de ruta
+  
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
   }
 
-  persons = persons.concat(person);
+  notes = notes.concat(note)
 
-  response.json(person);
-});
+  response.json(note);
+})
 
-
-
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>{
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 })
